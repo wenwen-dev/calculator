2eabcc4d424f41ada0b38bf0a8bd73a8a1705bfe
Wenwen Wang <wenwen.wangwen@gmail.com>
2022-09-23 11:18:49 -0700

    Complete calculation accepting 2 operands

--- script.js
+++ script.js
@@ -15,9 +15,41 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
-  if (operator === 'add') return add(a, b);
-  if (operator === 'subtract') return subtract(a, b);
-  if (operator === 'multiply') return multiply(a, b);
-  if (operator === 'divide') return divide(a, b);
+function operate(a, b, operator) {
+  if (operator === '+') return add(a, b);
+  if (operator === '-') return subtract(a, b);
+  if (operator === '*') return multiply(a, b);
+  if (operator === '/') return divide(a, b);
+}
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => button.addEventListener('click', processInput));
+
+const firstDisplayLine = document.querySelector('#display1');
+const secondDisplayLine = document.querySelector('#display2');
+
+let operands = [undefined,undefined, undefined];
+
+function processInput(e) {
+  const input = e.target.textContent;
+ 
+  if (input.match(/[0-9]/)) {
+    if (operands[0] === undefined)
+      operands[0] = Number(input);
+    else
+      operands[1] = Number(input);
+  }
+
+  if (input.match(/[-+--*/]/)) {
+    operands[2] = input;
+  }
+
+  if (input === '=') {
+    const result = operate(Number(operands[0]), Number(operands[1]), operands[2]);
+    secondDisplayLine.textContent = result;
+    operands = [undefined, undefined, undefined];
+  }
+
+  console.log(operands);
+  firstDisplayLine.textContent = e.target.textContent;
 }
