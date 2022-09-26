6c0c04821d870810780665f4d89a3f82bcf94fa8
Wenwen Wang <wenwen.wangwen@gmail.com>
2022-09-26 10:21:12 -0700

    Add styles

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  if (b === 0) return "divide by zero errorrrr";
-  return "a / b";
+  if (b === 0) return "divide by zero error";
+  return a / b;
 }
 
 function operate(a, b, operator) {
@@ -22,17 +22,40 @@ function operate(a, b, operator) {
   if (operator === '/') return divide(a, b);
 }
 
-const buttons = document.querySelectorAll('.btn');
-buttons.forEach(button => button.addEventListener('click', processInput));
-
 const firstDisplayLine = document.querySelector('#display1');
 const secondDisplayLine = document.querySelector('#display2');
 
-let operands = [undefined,undefined, undefined];
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => button.addEventListener('click', processInput));
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clear);
+
+let operands = [undefined, undefined, undefined, false];
+
+function clear(e) {
+  operands = [undefined, undefined, undefined, false];
+  firstDisplayLine.textContent = '';
+  secondDisplayLine.textContent = '';
+  console.log(operands);
+}
 
 function processInput(e) {
   let input = e.target.textContent;
- 
+
+  if (operands[3] === true) {
+    // const decimalLength = operands[0].split('.')[1].length;
+
+    if (operands[2] && operands[1] == undefined)
+      operands[1] = input * 0.1;
+    if (!operands[2] && operands[0] == undefined) {
+    console.log(Math.round(input * 0.1 * 10) / 10);
+      operands[0] = Math.round(Number(input) * 0.1) / 10;}
+    if (!operands[2] && operands[0] != undefined)
+      operands[0] += Number(input) * 0.1;
+  }
+
+//FIXME: if input is 0, it'll be falsy, which makes !operands[2] not work
   if (input.match(/[0-9]/)) {
     if (!operands[2] && (operands[0]))
       operands[0] = Number(operands[0] + input);
@@ -44,26 +67,41 @@ function processInput(e) {
       operands[1] = Number(input);
   }
 
-  if (input.match(/[\+\-\*-/]/)) {
+  if (input == '.') {
+    operands[3] = true;
+  }
+
+  if (input === '+' || input === '-' || input === '*' || input === '/') {
     if (operands[2]) {
       operands[0] = operate(Number(operands[0]), Number(operands[1]), operands[2]);
       firstDisplayLine.textContent = operands[0];
       secondDisplayLine.textContent = '';
       operands[1] = undefined;
+      operands[2] = input;
     }
     else
       operands[2] = input;
   }
-console.log('input: ' + input + typeof input);
+  
+
+  if (operands[2] && operands[0] == undefined && operands[1] == undefined) {
+  secondDisplayLine.textContent = 'Please select a number to get started'
+}
+
   if (input === '=') {
     const result = operate(Number(operands[0]), Number(operands[1]), operands[2]);
     secondDisplayLine.textContent = result;
     operands = [result, undefined, undefined];
   }
 
+  console.log('input: ' + input + typeof input);
   console.log(operands);
   firstDisplayLine.textContent += ' ' + e.target.textContent;
 }
 
-//TODO: 1. press '.', 2. chained operations, 3. invalid pressing sequence
-// 4. more operations available on a calculator such as % 5. prettier
+//TODO: press '.',
+//TODO: invalid pressing sequence
+//FIXME: division sign isn't right, result is always a/b
+// TODO: more operations available on a calculator such as % 
+//TODO: prettier
+//TODO: use RegEx for condition operator sign
