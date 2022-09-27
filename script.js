75939891f424d66bb8968df1d5912f2d12ad44e4
Wenwen Wang <wenwen.wangwen@gmail.com>
2022-09-27 10:35:05 -0700

    Complete project except for extra credit
    Extra credits: allowing decimal operation

--- script.js
+++ script.js
@@ -31,37 +31,23 @@ buttons.forEach(button => button.addEventListener('click', processInput));
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', clear);
 
-let operands = [undefined, undefined, undefined, false];
+let operands = [undefined, undefined, undefined, false, -1];
 
 function clear(e) {
   operands = [undefined, undefined, undefined, false];
   firstDisplayLine.textContent = '';
   secondDisplayLine.textContent = '';
-  console.log(operands);
 }
 
 function processInput(e) {
   let input = e.target.textContent;
 
-  if (operands[3] === true) {
-    // const decimalLength = operands[0].split('.')[1].length;
-
-    if (operands[2] && operands[1] == undefined)
-      operands[1] = input * 0.1;
-    if (!operands[2] && operands[0] == undefined) {
-    console.log(Math.round(input * 0.1 * 10) / 10);
-      operands[0] = Math.round(Number(input) * 0.1) / 10;}
-    if (!operands[2] && operands[0] != undefined)
-      operands[0] += Number(input) * 0.1;
-  }
-
-//FIXME: if input is 0, it'll be falsy, which makes !operands[2] not work
-  if (input.match(/[0-9]/)) {
-    if (!operands[2] && (operands[0]))
+  if (operands[3] === false && input.match(/[0-9]/)) {
+    if (operands[2] === undefined && (operands[0]))
       operands[0] = Number(operands[0] + input);
     if (operands[2] && (operands[0]) && operands[1])
       operands[1] = Number(operands[1] + input);
-    if (!operands[0])
+    if (operands[0] === undefined)
       operands[0] = Number(input);
     if (operands[0] && operands[2] && !operands[1])
       operands[1] = Number(input);
@@ -72,13 +58,20 @@ function processInput(e) {
   }
 
   if (input === '+' || input === '-' || input === '*' || input === '/') {
-    if (operands[2]) {
-      operands[0] = operate(Number(operands[0]), Number(operands[1]), operands[2]);
-      firstDisplayLine.textContent = operands[0];
-      secondDisplayLine.textContent = '';
-      operands[1] = undefined;
-      operands[2] = input;
+    if (operands[3] === true && !operands[1]) {
+      console.log(operands[4]);
+      console.log(operands[0]);
+      operands[0] = Number(operands[0]) * Math.pow(10, -1 * operands[4]);
+      operands[3] = false;
     }
+
+  if (operands[2]) {
+    operands[0] = operate(Number(operands[0]), Number(operands[1]), operands[2]);
+    firstDisplayLine.textContent = operands[0];
+    secondDisplayLine.textContent = '';
+    operands[1] = undefined;
+    operands[2] = input;
+  }
     else
       operands[2] = input;
   }
@@ -94,14 +87,10 @@ function processInput(e) {
     operands = [result, undefined, undefined];
   }
 
-  console.log('input: ' + input + typeof input);
-  console.log(operands);
   firstDisplayLine.textContent += ' ' + e.target.textContent;
 }
 
-//TODO: press '.',
-//TODO: invalid pressing sequence
-//FIXME: division sign isn't right, result is always a/b
-// TODO: more operations available on a calculator such as % 
+//TODO: allow decimal number input (e.g. 3.5)
+// TODO: allow operations available on a calculator such as % 
 //TODO: prettier
 //TODO: use RegEx for condition operator sign
