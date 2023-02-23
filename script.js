ea00302b7468b24eb7a80763f45add6d05ed7b56
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-23 11:13:38 -0800

    Use flexbox to have header and footer stick to ends

--- script.js
+++ script.js
@@ -36,6 +36,11 @@ let result;
 let waitingFor2ndNum;
 let isDecimal = false;
 
+//FIXME: Next time start from Extra credits "make it look nice"
+//TODO: make the page basic layout using flex, header, and footer pushed to bottom
+//make the operator button change color until another button is pressed
+//FIXME: at last, go through my codes, esp. storeNum() and improve the logic, can compare with other solutions to learn
+
 clearBtn.addEventListener('click', clear);
 numButtons.forEach(button => {
   button.addEventListener('click', displayNum);
